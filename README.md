7c1a9b6761f798a75e87be3a58256dc55953af81
Robotix703 <stephane.chatelle@orange.fr>
2023-03-17 21:09:03 +0100

    Add compressor

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# compressor_folder
-Compressor for a folder
+# Compressor_folder
+Compress all files in a folder
+
+## Usage
+```bash node compressor_folder.js <input> <output>```
---
+++ compressor_folder.js
@@ -0,0 +1,27 @@
+import fs from 'fs';
+import path from 'path';
+import sharp from 'sharp';
+
+const inputFolder = process.argv[2];
+const outputFolder = process.argv[3];
+console.log("Input folder : " + inputFolder);
+
+fs.readdir(inputFolder, (err, files) => {
+    if (err || files.length === 0) {
+        console.log(err);
+        return;
+    }
+    console.log("Nb file : " + files.length);
+
+    files.forEach(function (file) {
+        console.log("File : " + file);
+
+        sharp(path.join(inputFolder, file))
+            .jpeg({quality: 50})
+            .toFile(path.join(outputFolder, file), (err, info) => { 
+                if (err) {
+                    console.log(err);
+                }
+            });
+    });
+});
