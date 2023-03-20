95fcd241b312ecceec2812284c02b2c71dedcb55
Robotix703 <stephane.chatelle@orange.fr>
2023-03-20 20:25:11 +0100

    Add little fixes

--- compressor_folder.js
+++ compressor_folder.js
@@ -5,7 +5,9 @@ import sharp from 'sharp';
 const inputFolder = process.argv[2];
 const outputFolder = process.argv[3];
 console.log("Input folder : " + inputFolder);
+console.log("Output folder : " + outputFolder);
 
+//Compress
 fs.readdir(inputFolder, (err, files) => {
     if (err || files.length === 0) {
         console.log(err);
